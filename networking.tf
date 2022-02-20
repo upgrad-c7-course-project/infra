7569596ae43a350062e744b74374dce55d2198e8
Anees Syed <anees@Aneess-MacBook-Pro.local>
2022-02-20 13:21:59 -0800

    Setup networking

--- networking.tf
+++ networking.tf
@@ -1,7 +1,7 @@
 # VPC
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.12.0.0/16"
+  cidr_block = var.vpc-cidr-block
   enable_dns_hostnames = true
   tags = {
     Name = "101-vpc"
@@ -9,79 +9,79 @@ resource "aws_vpc" "main" {
 }
 
 
-# # subnets
+# subnets
 
-# resource "aws_subnet" "public-subnet1" {
-#   vpc_id = aws_vpc.main.id
-#   cidr_block = "10.12.1.0/24"
-#   availability_zone = "us-east-1a"
-#   # map_public_ip_on_launch = true
-#   tags = {
-#     Name = "101-public-subnet1"
-#   }
-# }
-
-
-# resource "aws_subnet" "private-subnet1" {
-#   vpc_id = aws_vpc.main.id
-#   cidr_block = "10.12.2.0/24"
-#   availability_zone = "us-east-1a"
-#   tags = {
-#     Name = "101-private-subnet1"
-#   }
-# }
+resource "aws_subnet" "public-subnet1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.12.1.0/24"
+  availability_zone = var.aws-region-az-a
+  # map_public_ip_on_launch = true
+  tags = {
+    Name = "101-public-subnet1"
+  }
+}
 
 
-
-# resource "aws_subnet" "public-subnet2" {
-#   vpc_id = aws_vpc.main.id
-#   cidr_block = "10.12.3.0/24"
-#   availability_zone = "us-east-1b"
-#   tags = {
-#     Name = "101-public-subnet2"
-#   }
-# }
-
-
-# resource "aws_subnet" "private-subnet2" {
-#   vpc_id = aws_vpc.main.id
-#   cidr_block = "10.12.4.0/24"
-#   availability_zone = "us-east-1b"
-#   tags = {
-#     Name = "101-private-subnet2"
-#   }
-# }
+resource "aws_subnet" "private-subnet1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.12.2.0/24"
+  availability_zone = var.aws-region-az-a
+  tags = {
+    Name = "101-private-subnet1"
+  }
+}
 
 
-# # IGW
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = aws_vpc.main.id
 
-#   tags = {
-#     Name = "101-igw"
-#   }
-# }
+resource "aws_subnet" "public-subnet2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.12.3.0/24"
+  availability_zone = var.aws-region-az-b
+  tags = {
+    Name = "101-public-subnet2"
+  }
+}
 
 
-# # NAT
-# resource "aws_eip" "ngw-ip" {
-#   vpc      = true
+resource "aws_subnet" "private-subnet2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.12.4.0/24"
+  availability_zone = var.aws-region-az-b
+  tags = {
+    Name = "101-private-subnet2"
+  }
+}
 
-#   tags = {
-#     Name = "101-ngw-ip"
-#   }
-# }
 
-# resource "aws_nat_gateway" "ngw" {
-#   allocation_id = aws_eip.ngw-ip.id
-#   subnet_id     = aws_subnet.public-subnet1.id
+# IGW
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
-#   tags = {
-#     Name = "101-ngw"
-#   }
+  tags = {
+    Name = "101-igw"
+  }
+}
 
-#   # To ensure proper ordering, it is recommended to add an explicit dependency
-#   # on the Internet Gateway for the VPC.
-#   depends_on = [aws_internet_gateway.igw]
-# }
+
+# NAT
+resource "aws_eip" "ngw-ip" {
+  vpc      = true
+
+  tags = {
+    Name = "101-ngw-ip"
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw-ip.id
+  subnet_id     = aws_subnet.public-subnet1.id
+
+  tags = {
+    Name = "101-ngw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
 
--- route-tables.tf
+++ route-tables.tf
@@ -1,57 +1,57 @@
-# # -- Public Route
+# -- Public Route
 
-# resource "aws_route_table" "public-route" {
-#   vpc_id = aws_vpc.main.id
+resource "aws_route_table" "public-route" {
+  vpc_id = aws_vpc.main.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
+  route {
+    cidr_block = var.cidr-all
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
-#   tags = {
-#     Name = "101-public-route"
-#   }
-# }
-
-
-
-# resource "aws_route_table_association" "rt-association-pub1" {
-#   subnet_id      = aws_subnet.public-subnet1.id
-#   route_table_id = aws_route_table.public-route.id
-# }
-
-
-# resource "aws_route_table_association" "rt-association-pub2" {
-#   subnet_id      = aws_subnet.public-subnet2.id
-#   route_table_id = aws_route_table.public-route.id
-# }
-
-
-# # -- Private Route
-
-
-# resource "aws_route_table" "private-route" {
-#   vpc_id = aws_vpc.main.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.ngw.id
-#   }
-
-#   tags = {
-#     Name = "101-private-route"
-#   }
-# }
+  tags = {
+    Name = "101-public-route"
+  }
+}
 
 
 
-# resource "aws_route_table_association" "rt-association-pri1" {
-#   subnet_id      = aws_subnet.private-subnet1.id
-#   route_table_id = aws_route_table.private-route.id
-# }
+resource "aws_route_table_association" "rt-association-pub1" {
+  subnet_id      = aws_subnet.public-subnet1.id
+  route_table_id = aws_route_table.public-route.id
+}
 
 
-# resource "aws_route_table_association" "rt-association-pri2" {
-#   subnet_id      = aws_subnet.private-subnet2.id
-#   route_table_id = aws_route_table.private-route.id
-# }
+resource "aws_route_table_association" "rt-association-pub2" {
+  subnet_id      = aws_subnet.public-subnet2.id
+  route_table_id = aws_route_table.public-route.id
+}
+
+
+# -- Private Route
+
+
+resource "aws_route_table" "private-route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = var.cidr-all
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = "101-private-route"
+  }
+}
+
+
+
+resource "aws_route_table_association" "rt-association-pri1" {
+  subnet_id      = aws_subnet.private-subnet1.id
+  route_table_id = aws_route_table.private-route.id
+}
+
+
+resource "aws_route_table_association" "rt-association-pri2" {
+  subnet_id      = aws_subnet.private-subnet2.id
+  route_table_id = aws_route_table.private-route.id
+}
